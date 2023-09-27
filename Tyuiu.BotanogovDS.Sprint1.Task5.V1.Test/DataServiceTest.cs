bae50cb0cf3b4ba05276280766c68282ffcef597
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-09-27 22:40:49 +0500

    Task5

---
+++ Tyuiu.BotanogovDS.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.BotanogovDS.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.BotanogovDS.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestDistanceBetweenDots()
+        {
+            DataService dataService = new DataService();
+            double x1 = 1;
+            double y1 = 1;
+            double x2 = 4;
+            double y2 = 5;
+
+            int distance = dataService.DistanceBetweenDots(x1, y1, x2, y2);
+
+            Assert.AreEqual(5, distance);
+        }
+    }
+}
